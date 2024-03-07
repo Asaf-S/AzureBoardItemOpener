8df0ace0c1badecef72cba3f1597443ef48e914e
Asaf <AsafShifer@Gmail.com>
2024-03-07 21:45:13 +0200

    Settings

--- src/background.js
+++ src/background.js
@@ -1,6 +1,34 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "openUrl") {
-    const url = `https://abc.com?id=${request.number}`;
-    chrome.tabs.create({ url });
+const DEFAULT_URL = "https://dev.azure.com/org/project/_workitems/edit/{{input}}"; // Default URL
+
+async function getStoredUrl() {
+  try {
+    const storedUrl = await chrome.storage.sync.get("url");
+    const result = storedUrl.url || DEFAULT_URL;
+    console.log(`bg.getStoredUrl=${result}`);
+    return result;
+  } catch (e) {
+    console.error(e);
   }
+}
+
+async function storeUrl(url) {
+  const parsedURL = url || DEFAULT_URL;
+  console.log(`bg.storeUrl=${url}, parsedURL=${parsedURL}`);
+  await chrome.storage.sync.set({ url: parsedURL });
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  (async () => {
+    if (request.action === "openUrl") {
+      const url = await getStoredUrl();
+      const finalUrl = url.replace("{{input}}", request.number);
+      chrome.tabs.create({ url: finalUrl });
+    } else if (request.action === "setUrl") {
+      await storeUrl(request.url);
+    } else if (request.action === "getUrl") {
+      const url = await getStoredUrl();
+      sendResponse({ url });
+    }
+  })();
+  return true;
 });
--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,21 @@
-document.getElementById("submitButton").addEventListener("click", () => {
-  const numberInput = document.getElementById("numberInput");
+const settings = document.getElementById("settings");
+const numberInput = document.getElementById("numberInput");
+const submitButton = document.getElementById("submitButton");
+const settingsButton = document.getElementById("settingsButton");
+const urlInput = document.getElementById("urlInput");
+const saveUrlButton = document.getElementById("saveUrlButton");
+const resetUrlButton = document.getElementById("resetUrlButton");
+
+// Setting click
+settingsButton.addEventListener("click", async () => {
+  settings.classList.toggle("hidden");
+
+  const url = await getStoredUrl();
+  urlInput.value = url;
+  console.log(`url=${url}`);
+});
+
+submitButton.addEventListener("click", async () => {
   const number = numberInput.value;
   if (number) {
     chrome.runtime.sendMessage({ action: "openUrl", number });
@@ -8,3 +24,41 @@ document.getElementById("submitButton").addEventListener("click", () => {
     alert("Please enter a number");
   }
 });
+
+saveUrlButton.addEventListener("click", async () => {
+  const url = urlInput.value;
+  if (url) {
+    setStoredUrl(url);
+    // settings.classList.add("hidden");
+  } else {
+    alert("Please enter a valid URL");
+  }
+});
+
+resetUrlButton.addEventListener("click", async () => {
+  if (confirm("Are you sure you want to reset the URL?")) {
+    await setStoredUrl(); // No URL entered
+
+    const url = await getStoredUrl();
+    urlInput.value = url;
+    console.log(`url=${url}`);
+  } else {
+    console.log(`pop.resetUrlButton.click = canceled`);
+  }
+});
+
+// Initial setup to get the stored URL and update the input field
+window.addEventListener("load", async () => {
+  const url = await getStoredUrl();
+  urlInput.value = url;
+  console.log(`url=${url}`);
+});
+
+async function getStoredUrl() {
+  const response = await chrome.runtime.sendMessage({ action: "getUrl" });
+  return response.url;
+}
+
+async function setStoredUrl(url) {
+  chrome.runtime.sendMessage({ action: "setUrl", url });
+}
